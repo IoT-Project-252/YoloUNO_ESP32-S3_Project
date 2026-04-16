5750376091b31646c6fb1b0e91450f7dc4aaeed1
TanLee24 <tandepzai24@gmail.com>
2026-04-16 09:01:33 +0700

    feat: initialize shared datas

--- Src/include/global.h
+++ Src/include/global.h
@@ -6,17 +6,14 @@
 #include "freertos/semphr.h"
 #include "freertos/task.h"
 
-// Gom tất cả dữ liệu cần chia sẻ và các công cụ đồng bộ (Semaphore/Mutex) vào một struct
+// Struct for Shared Variables
 struct SensorData 
 {
-    // Dữ liệu cảm biến
     float temperature;
     float humidity;
     
-    // Mutex để bảo vệ biến nhiệt độ, độ ẩm khi các task truy cập
     SemaphoreHandle_t mutex;
     
-    // Các Binary Semaphore để báo hiệu 3 trạng thái lên màn hình LCD
     SemaphoreHandle_t semNormal;
     SemaphoreHandle_t semWarning;
     SemaphoreHandle_t semCritical;
